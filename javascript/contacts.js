204eb01769c002b2167c22f51ede42ccf291c278
mister-serg <zsdp2010@yandex.ru>
2025-08-31 15:35:17 +0300

    added a file contacts.html

--- javascript/contacts.js
+++ javascript/contacts.js
@@ -1,33 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const burger = document.querySelector(".burger");
-//   const menuAdaptive = document.querySelector(".menu-adaptive");
-
-//   function toggleMenu() {
-//     if (menuAdaptive.style.display === "flex") {
-//       menuAdaptive.style.display = "none"; // Скрываем меню
-//     } else {
-//       menuAdaptive.style.display = "flex"; // Показываем меню
-//     }
-//   }
-
-//   function checkScreenSize() {
-//     if (window.innerWidth >= 0 && window.innerWidth <= 1024) {
-//       burger.style.display = "block"; // Показываем гамбургер на малых экранах
-//     } else {
-//       burger.style.display = "none"; // Скрываем гамбургер на больших экранах
-//       menuAdaptive.style.display = "none"; // Обязательно прячем меню на больших экранах
-//     }
-//   }
-
-//   // Первоначальная проверка размеров экрана при загрузке страницы
-//   checkScreenSize();
-
-//   // Нажатие на бургур-меню
-//   burger.addEventListener("click", toggleMenu);
-
-//   // Проверка изменений размеров окна
-//   window.addEventListener("resize", checkScreenSize);
-// });
 document.addEventListener("DOMContentLoaded", () => {
   const burger = document.querySelector(".burger");
   const menuAdaptive = document.querySelector(".menu-adaptive");
@@ -66,3 +36,33 @@ document.addEventListener("DOMContentLoaded", () => {
   // Проверка изменений размеров окна
   window.addEventListener("resize", checkScreenSize);
 });
+
+// Создание карты
+const screenWidth = window.innerWidth;
+
+// Создание карты с адаптивным масштабом
+let zoomLevel;
+if (screenWidth >= 1920) {
+  zoomLevel = 17;
+} else if (screenWidth >= 1024) {
+  zoomLevel = 16.4;
+} else if (screenWidth >= 768) {
+  zoomLevel = 20;
+} else {
+  zoomLevel = 20; // Значение по умолчанию для меньших экранов
+}
+
+const map = L.map('map').setView([48.86091, 2.3364], zoomLevel);
+
+// Добавление тайлов (фонов карты)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+// Добавление маркеров
+const marker1 = L.marker([48.86091, 2.3364]).addTo(map);
+const marker2 = L.marker([48.8602, 2.3333]).addTo(map);
+const marker3 = L.marker([48.8607, 2.3397]).addTo(map);
+const marker4 = L.marker([48.8619, 2.333]).addTo(map);
+const marker5 = L.marker([48.8625, 2.3365]).addTo(map);
--- javascript/index.js
+++ javascript/index.js
@@ -60,62 +60,10 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   const circleElement = document.querySelector(
-//     ".explore-img-child-line-circle"
-//   );
-//   const exploreImgChild = document.querySelector(".explore-img-child");
-//   const exploreImgEre = document.querySelector(".explore-img-ere");
-
-//   let dragging = false;
-//   let startX, clientX, originalLeft;
-
-//   // Размеры окружности и контейнера
-//   const circleRect = circleElement.getBoundingClientRect();
-//   const parentWidth = exploreImgChild.parentNode.offsetWidth;
-
-//   // Начало движения (при нажатии мышью)
-//   circleElement.addEventListener("mousedown", (e) => {
-//     dragging = true;
-//     startX = e.clientX;
-//     originalLeft = parseFloat(getComputedStyle(exploreImgChild).left) || 0;
-//     circleElement.style.cursor = "grabbing";
-//   });
-
-//   // Перемещение мыши
-//   document.addEventListener("mousemove", (e) => {
-//     if (!dragging) return;
-
-//     // Получаем новые координаты перемещения
-//     const deltaX = e.clientX - startX;
-//     const newLeft = originalLeft + deltaX;
-
-//     // Ограничиваем перемещение элементами контейнера
-//     const boundedLeft = Math.max(
-//       0, // Минимальная позиция слева
-//       Math.min(newLeft, parentWidth - circleRect.width / 6) // Максимальная позиция справа
-//     );
-
-//     // Применяем новую позицию
-//     exploreImgChild.style.left = `${boundedLeft}px`;
-
-//     // Пересчёт clip-path относительно центра окружности
-//     const centerOfCircle = boundedLeft + circleRect.width / 5; // НАСТОЯЩИЙ ЦЕНТР ОКРУЖНОСТИ
-//     const relativeCenter = centerOfCircle / parentWidth;
-//     const centerPointPercent = relativeCenter * 100 + "%";
-
-//     exploreImgEre.style.clipPath = `polygon(0 0, ${centerPointPercent} 0, ${centerPointPercent} 100%, 0 100%)`;
-//   });
-
-//   // Завершение движения (отпускаем кнопку мыши)
-//   document.addEventListener("mouseup", () => {
-//     dragging = false;
-//     circleElement.style.cursor = '';
-//   });
-// });
-
 document.addEventListener("DOMContentLoaded", () => {
-  const circleElement = document.querySelector(".explore-img-child-line-circle");
+  const circleElement = document.querySelector(
+    ".explore-img-child-line-circle"
+  );
   const exploreImgChild = document.querySelector(".explore-img-child");
   const exploreImgEre = document.querySelector(".explore-img-ere");
 
@@ -130,10 +78,9 @@ document.addEventListener("DOMContentLoaded", () => {
   function handleStart(event) {
     event.preventDefault(); // Предотвращаем стандартные реакции браузера
     dragging = true;
-    startX =
-      event.type.includes("touch") // Если событие касается (touch)
-        ? event.touches[0].pageX // Берём координату касания
-        : event.pageX; // Иначе берём координату мыши
+    startX = event.type.includes("touch") // Если событие касается (touch)
+      ? event.touches[0].pageX // Берём координату касания
+      : event.pageX; // Иначе берём координату мыши
     originalLeft = parseFloat(getComputedStyle(exploreImgChild).left) || 0;
     circleElement.style.cursor = "grabbing";
   }
@@ -142,10 +89,9 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!dragging) return;
     event.preventDefault(); // Предотвращаем прокрутку страницы
 
-    const clientXValue =
-      event.type.includes("touch") // Если событие касается (touch)
-        ? event.touches[0].pageX // Берём координату касания
-        : event.pageX; // Иначе берём координату мыши
+    const clientXValue = event.type.includes("touch") // Если событие касается (touch)
+      ? event.touches[0].pageX // Берём координату касания
+      : event.pageX; // Иначе берём координату мыши
 
     const deltaX = clientXValue - startX;
     const newLeft = originalLeft + deltaX;
@@ -182,6 +128,23 @@ document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("touchmove", handleMove, { passive: false });
   document.addEventListener("touchend", handleEnd);
 });
+
+// Создание карты
+const screenWidth = window.innerWidth;
+
+// Создание карты с адаптивным масштабом
+let zoomLevel;
+if (screenWidth >= 1920) {
+  zoomLevel = 17;
+} else if (screenWidth >= 1024) {
+  zoomLevel = 16.4;
+} else if (screenWidth >= 768) {
+  zoomLevel = 20;
+} else {
+  zoomLevel = 15; // Значение по умолчанию для меньших экранов
+}
+
+const map = L.map('map').setView([48.86091, 2.3364], zoomLevel);
 
 // Добавление тайлов (фонов карты)
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
